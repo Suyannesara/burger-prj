a6e4c4c997cad2a1c6129710422fed0fb8fd35cd
suyanneSara <suyannesara17@gmail.com>
2022-05-28 14:21:19 -0300

    Configured routes and organized project structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/">
+                <img id="logo" src="" alt="Project Logo">
+            </router-link>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/requests">Requests</router-link>
+        </nav>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<style>
+</style>
