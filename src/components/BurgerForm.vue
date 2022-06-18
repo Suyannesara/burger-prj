f6467499d7f8a33f93323410a7828ce0af5dc5db
suyanneSara <suyannesara17@gmail.com>
2022-06-18 19:03:21 -0300

    Pushing data from the API to the front-form

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -6,31 +6,31 @@
                 <div class="input-container">
                     <label for="name">Client name</label>
                     <input v-model="name" id="name" name="name" type="text" placeholder="Type your name">
-                </div> 
+                </div>
 
                 <div class="input-container">
-                   <label for="bread">Choose the bread</label>
-                   <select v-model="bread" name="bread" id="bread">
+                    <label for="bread">Choose the bread</label>
+                    <select v-model="bread" name="bread" id="bread">
                         <option value="">Select Bread</option>
-                        <option value="">Integral</option>
-                   </select>
-                </div> 
+                        <option v-for="bread in loaves" :key="bread.id" :value="bread.tipo">{{ bread.tipo }}</option>
+                    </select>
+                </div>
 
                 <div class="input-container">
-                   <label for="meat">Choose the Meat</label>
-                   <select v-model="meat" name="meat" id="meat">
+                    <label for="meat">Choose the Meat</label>
+                    <select v-model="meat" name="meat" id="meat">
                         <option value="">Select Meat</option>
-                        <option value="">Maminha</option>
-                   </select>
-                </div> 
+                        <option v-for="meat in meats" :key="meat.id" :value="meat.tipo">{{ meat.tipo }}</option>
+                    </select>
+                </div>
 
                 <div id="optionals container" class="input-container">
-                   <label id="optionals-title" for="optionals">Choose the optionals</label>
-                   <div class="checkbox-container">
-                       <input v-model="optionals" type="checkbox" name="optionals" value="salame">
-                       <span>Salame</span>
-                   </div>
-                </div> 
+                    <label id="optionals-title" for="optionals">Choose the optionals</label>
+                    <div class="checkbox-container" v-for="optional in optionals" :key="optional.id" :value="optional.tipo">
+                        <input v-model="optionals" type="checkbox" name="optionals" :value="optional.tipo">
+                        <span> {{optional.tipo}}</span>
+                    </div>
+                </div>
 
                 <div class="input-container">
                     <input class="submit-btn" type="submit" value="create-my-burger">
@@ -43,81 +43,111 @@
 </template>
 
 <script>
-    export default {
-        name: 'BurgerForm'
+
+export default {
+    name: 'BurgerForm',
+    data() {
+        return {
+            loaves: null,
+            meats: null,
+            optionals: null,
+            name: null,
+            bread: null,
+            meat: null,
+            optionals: [],
+            status: "Solicitado",
+            msg: null
+
+
+        }
+    },
+    methods: {
+        async getIngredients() {
+            const req = await fetch("http://localhost:3000/ingredientes")
+            const data = await req.json()
+
+            this.loaves = data.paes
+            this.meats = data.carnes
+            this.optionals = data.opcionais
+        }
+    },
+    mounted(){
+        //When the app be mounted, execute the functions
+        this.getIngredients()
     }
+}
 </script>
 
 <style scoped>
-    #burger-form {
-        max-width: 400px;
-        margin: 0 30%;
-    }
+#burger-form {
+    max-width: 400px;
+    margin: 0 30%;
+}
 
-    .input-container {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-    }
+.input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
 
-    label {
-        font-weight: bold;
-        margin-bottom: 15px;
-        color: #222;
-        padding: 5px 10px;
-        border-left: 4px solid #FCBA03;
-    }
+label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #FCBA03;
+}
 
-    input, select {
-        padding: 5px 10px;
-        width: 300px;
-    }
+input,
+select {
+    padding: 5px 10px;
+    width: 300px;
+}
 
-    #optionals-container {
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
+#optionals-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+}
 
-    #optionals-title {
-        width: 100%;
-    }
+#optionals-title {
+    width: 100%;
+}
 
-    .checkbox-container {
-        display: flex;
-        align-items: flex-start;
+.checkbox-container {
+    display: flex;
+    align-items: flex-start;
 
-        width: 50%;
-        margin-bottom: 20px;
-    }
+    width: 50%;
+    margin-bottom: 20px;
+}
 
-    .checkbox-container span,
-    .checkbox-container input {
-        width: auto;
-    }
+.checkbox-container span,
+.checkbox-container input {
+    width: auto;
+}
 
-    .checkbox-container span {
-        margin-left: 6px;
-        font-weight: bold;
-    }
+.checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+}
 
-    .submit-btn {
-        background-color: #222;
-        color: #FCBA03;
-        font-weight: bold;
-        border: 2px solid #222;
-        border-radius: 4px;
-        font-size: 16px;
+.submit-btn {
+    background-color: #222;
+    color: #FCBA03;
+    font-weight: bold;
+    border: 2px solid #222;
+    border-radius: 4px;
+    font-size: 16px;
 
-        padding: 10px;
-        margin: 0 auto;
+    padding: 10px;
+    margin: 0 auto;
 
-        cursor: pointer;
-        transition: .5s;
-    }
+    cursor: pointer;
+    transition: .5s;
+}
 
-    .submit-btn:hover {
-        background-color: transparent;
-        color: #222;
-    }
-
+.submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+}
 </style>
